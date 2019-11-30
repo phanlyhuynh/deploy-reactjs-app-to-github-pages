d71a5d98e38ed909ded3928eee52fd762028ad3d
phanlyhuynh <32766920+phanlyhuynh@users.noreply.github.com>
2019-11-30 17:42:22 +0000

    Deploying to gh-pages from master 71bdfa49ee6fc7812dab4b9119ccae2060c975b1

---
+++ precache-manifest.20ddc80c512515ea2971daec68759b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8e14b0ef45bebd2ad08b98c074c4ab",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ab29541ca3285b012604",
+    "url": "/test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c76d4ebf5d0a4751a5ed",
+    "url": "/test/static/js/2.ea524fe0.chunk.js"
+  },
+  {
+    "revision": "ab29541ca3285b012604",
+    "url": "/test/static/js/main.aee67fae.chunk.js"
+  },
+  {
+    "revision": "8775a232544611850a4e",
+    "url": "/test/static/js/runtime-main.d3de54f7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test/static/media/logo.25bf045c.svg"
+  }
+]);
