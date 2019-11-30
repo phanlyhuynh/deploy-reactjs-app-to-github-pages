e77e150f90ef3d0c660af30385436983b3d927f4
phanlyhuynh <32766920+phanlyhuynh@users.noreply.github.com>
2019-11-30 17:54:09 +0000

    Deploying to gh-pages from master 156209792369e85e9cf0def3d45b15957bef170f

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.20ddc80c512515ea2971daec68759b09.js"
+  "/deploy-reactjs-app-to-github-pages/precache-manifest.882ef2c17d474340337c3179d1cd62a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-reactjs-app-to-github-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
